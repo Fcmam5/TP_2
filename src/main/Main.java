ad443ce0604c379f13f7a45ef34c2c514a677024
ilyes hammadi <hammadiilyesahmed@gmail.com>
2015-09-27 11:47:33 +0100

    stable version of Compte

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
+import main.model.Distributeur;
+
 /**
  * Created by ilyes on 27/09/15.
  */
 public class Main {
 
     public static void main(String[] args) {
-
+        Distributeur.menu();
     }
 }
--- src/main/model/Compte.java
+++ src/main/model/Compte.java
@@ -1,31 +1,23 @@
 package main.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * Created by ilyes on 27/09/15.
  */
 public class Compte {
 
-    private int numCompte;
+    private static int numCompte = 0;
     private double solde;
     private double taux;
 
 
-    public Compte(int numCompte, double solde, double taux) {
-        this.numCompte = numCompte;
+    public Compte(double solde, double taux) {
+        Compte.numCompte++;
         this.solde = solde;
         this.taux = taux;
     }
 
     public int getNumCompte() {
         return numCompte;
-    }
-
-    public void setNumCompte(int numCompte) {
-        this.numCompte = numCompte;
     }
 
     public double getSolde() {
@@ -45,48 +37,27 @@ public class Compte {
     }
 
 
-    @Override
-    public String toString() {
-        return "le Compte n: " + numCompte + " Son solde est de : " + solde;
-    }
-
     public double calculInteret(int taux) {
         return (solde * taux / 100);
-
     }
 
-    InputStreamReader lecture = new InputStreamReader(System.in);
-    BufferedReader clavier = new BufferedReader(lecture);
-
-    /***
-     * @param montant
-     * @param choice
-     */
-    public void modification(double montant, int choice) throws IOException {
-
-
-        int choix = Integer.parseInt(clavier.readLine());
-        switch (choix) {
-            //ajout
-            case 1:
-                solde += montant;
-                break;
-            //retirer
-            case 2:
-                solde -= montant;
-                break;
-        }
-
+    @Override
+    public String toString() {
+        return "le Compte n: " + numCompte + " Son solde est de : " + (solde - calculInteret((int) taux));
     }
 
-//    public void augmenter(double montant){
-//        solde
-//    }
 
-
-    double calculInteret() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void augmenter(double montant) {
+        solde += montant;
     }
+
+    public double retirer(double montant) {
+        if (solde >= montant) {
+            return solde -= montant;
+        } else
+            return -1;
+    }
+
 
 
 }
