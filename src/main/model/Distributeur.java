888c7b40802dd582a63058f61106ec73208eb877
Fcmam5 <dr.fcmam5@gmail.com>
2015-09-27 11:07:56 +0100

    Distrubuteur class added

--- src/main/model/Distributeur.java
+++ src/main/model/Distributeur.java
@@ -1,8 +1,19 @@
 package main.model;
-
+import main.model.Compte;
 /**
  * Created by fcmam5 on 27/09/15.
  */
 public class Distributeur {
+    /** afficher
+     * consulter
+     * ajout
+     * retire
+     */
+    static void menu(byte choix){
+        switch (choix){
+            case 1:
+                System.out.println("");
+        }
+    }
 
 }
